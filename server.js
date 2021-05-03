c295aacb6ca9d276405f6e6575eea5bb219200e0
AyahZaareer <ayah@LTUCSTUDENT-L075.localdomain>
2021-05-04 01:25:06 +0300

    express2

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express')
 const cors = require('cors')
 const data = require('./asesst/data/weather.json')
 const { json } = require('express')
-// require('dotenv').config()
+require('dotenv').config()
 
 
-
+let PORT = process.env.PORT
 
 const app = express()
 app.use(cors())
 app.get('/', function (req, res) {
   res.send(data)
 })
-app.listen(3030)
+app.listen(PORT)
